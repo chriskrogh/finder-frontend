22c689eeb0149cabf532e60fd78aa8ab47980f47
Christopher Krogh <chris.krogh@outlook.com>
2021-05-02 09:56:49 -0400

    updated the server url

--- src/utils/providers/ApolloClient.ts
+++ src/utils/providers/ApolloClient.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: process.env.REACT_APP_SERVER_URL,
   cache: new InMemoryCache(),
 });
