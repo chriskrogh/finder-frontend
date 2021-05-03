166cb32e6873d6d874174d4b4a56c554c39c6e78
Christopher Krogh <chris.krogh@outlook.com>
2021-05-02 23:36:56 -0400

    update light theme

--- src/theme/colors/light.ts
+++ src/theme/colors/light.ts
@@ -2,9 +2,9 @@ import { ThemeColors } from './types';
 
 export const LightThemeColors: ThemeColors = {
   background: '#FFF',
-  primary: '',
-  secondary: '',
-  tertiary: '',
+  primary: '#EF5DA8',
+  secondary: '#5D5FEF',
+  tertiary: '#FCDDEC',
   text: '#000',
   subtext: '#818181',
 };
--- src/theme/typography/types.ts
+++ src/theme/typography/types.ts
@@ -1,4 +1,4 @@
-type ThemeTag = {
+export type ThemeTag = {
   fontFamily: string;
   fontSize: number;
   fontWeight: number;
