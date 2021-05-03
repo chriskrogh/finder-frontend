a48c9068dc5a5b15ba6a65a113e5afd034827be8
Christopher Krogh <chris.krogh@outlook.com>
2021-05-02 23:36:28 -0400

    position relative on page

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 const Page = styled.div`
+  position: relative;
   width: 100vw;
   height: ${window.innerHeight}px;
   background-color: ${({ theme }) => theme.colors.background};
